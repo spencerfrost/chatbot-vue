254d6e5d43bf247cf8cf0b31454c82c2c86f6956
Spencer <spencer.frost@eventconnect.io>
2023-06-14 12:35:30 -0400

    Implement vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-// Not sure why Typescript is complaining about this import... Need to investigate.
 import App from './App.vue'
-import Chat from './components/Chat.vue'
-import Upload from './components/Upload.vue'
-// import Store from './components/Store.vue'
-
+import router from './router.js'
 import './style.css'
-
-const routes = [
-  { path: '/chat', component: Chat },
-  { path: '/upload', component: Upload },
-  // { path: '/store', component: Store },
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
 
 createApp(App).use(router).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Chat from './components/Chat.vue'
+import Upload from './components/Upload.vue'
+import Store from './components/Store.vue'
+
+const routes = [
+  { path: '/chat', component: Chat },
+  { path: '/upload', component: Upload },
+  { path: '/store', component: Store },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router;
